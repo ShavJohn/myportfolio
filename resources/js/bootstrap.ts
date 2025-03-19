6e1e2412d6e02a74862b1620f75eddcabae89179
ShavJohn <shavarsh.garabedian@gmail.com>
2025-03-19 16:34:47 +0200

    Second commit

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,11 @@
-import axios from 'axios';
-window.axios = axios;
+import axios, { AxiosStatic} from 'axios';
+
+declare global {
+    interface Window {
+        axios: AxiosStatic
+    }
+}
+
+window.axios = axios as AxiosStatic;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
