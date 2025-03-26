07e4cc976b72c3d8ebfc2df29d89d1053c46ddd0
ShavJohn <shavarsh.garabedian@gmail.com>
2025-03-27 01:02:42 +0200

    Added contact us page

--- resources/js/Views/Contact-me.vue
+++ resources/js/Views/Contact-me.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-
+import ContactMe from "../components/contactme/contact-me.vue"
 
 </script>
 
 <template>
     <div>
-
+        <contact-me></contact-me>
     </div>
 </template>
 
--- resources/js/components/contactme/contact-me.vue
+++ resources/js/components/contactme/contact-me.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
-
+let imagePrefix = window.imagePrefix
 
 </script>
 
 <template>
-    <div>
-
+    <div class="contact-me-container">
+        <div class="contact-me-inner-container">
+            <div class="image-container">
+                <img ref="image" :src="`${imagePrefix}/shavjohn_logo.png`" alt="3D Hover Effect"  />
+            </div>
+            <div class="form-container">
+                <form>
+                    <div class="form-input">
+                        <label for="your-name">Your Name</label>
+                        <input type="text" id="your-name" placeholder="Your Name">
+                    </div>
+                    <div class="form-input">
+                        <label for="your-email">Your Email</label>
+                        <input type="email" id="your-email" placeholder="Your Email">
+                    </div>
+                    <div class="form-input">
+                        <label>Your Message</label>
+                        <textarea></textarea>
+                    </div>
+                    <div class="form-input">
+                        <button>Send Message</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
